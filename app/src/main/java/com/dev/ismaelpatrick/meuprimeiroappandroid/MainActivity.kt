faf90a1f67d0f82e7ccf9604f884dd3bbfe7dc95
ismaelp96 <ismael6991@live.com>
2026-03-30 14:59:43 -0300

    Vinculando Views em activities

--- app/src/main/java/com/dev/ismaelpatrick/meuprimeiroappandroid/MainActivity.kt
+++ app/src/main/java/com/dev/ismaelpatrick/meuprimeiroappandroid/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.dev.ismaelpatrick.meuprimeiroappandroid
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.dev.ismaelpatrick.meuprimeiroappandroid.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+   private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        with(binding.tvMyFirstAndroidApp) {
+            this?.text = "Meu Primeiro App Android!"
+            this?.textAlignment = TextView.TEXT_ALIGNMENT_CENTER
+        }
+
     }
 }
